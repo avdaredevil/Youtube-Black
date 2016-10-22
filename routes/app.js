8109689b03862023531f7cde9122138a8b2bb8f2
Apoorv Verma [AP] <avdaredevil@gmail.com>
2016-10-22 17:51:31 -0400

    Updated a crap load

--- routes/app.js
+++ routes/app.js
@@ -1,6 +1,17 @@
 var express = require('express');
+var path = require('path');
+var fs = require('fs');
 var router = express.Router();
 
-router.get('/', (req, res) => res.render('index', {question: "Sample Poll"}));
+router.get('/', (req, res) => res.render('index'));
+
+router.get('/Get-Component/:component', (req, res) => {
+    const name = req.params.component.replace(/\\\//g,''), loc = path.join('components',name), fullLoc = path.join(express().get("views"),loc)
+    fs.access(fullLoc+".pug", fs.F_OK, function(err) {
+        if (err) {return res.status(404).json({message: "No such template"})}
+        res.render(loc)
+    })
+})
+
 
 module.exports = router;
