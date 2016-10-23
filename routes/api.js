3263fb8d25e0ea3fda6af9714cce63ea21fab181
Apoorv Verma [AP] <avdaredevil@gmail.com>
2016-10-23 01:16:10 -0400

    Updated Code

--- routes/api.js
+++ routes/api.js
@@ -4,15 +4,16 @@ var config  = require("../config/keys.json");
 var querystring = require("querystring");
 
 var router   = express.Router();
+const MakeJSON = j => typeof j=="object"?j:JSON.parse(j)
 const API = {
     key: config.google,
     base: "https://www.googleapis.com/youtube/v3/",
     get: (route,data) => new Promise((res,rej) => {
         data.key = API.key
-        const q = querystring.stringify(data), URL = API.base()+route+"?"+q
+        const q = querystring.stringify(data), URL = API.base+route+"?"+q
         console.log("URL:",URL)
-        request.get(URL, (err,resp,body) => err?rej(err):res(body))
-    }),
+        request.get(URL, (err,resp,body) => err?rej(err):res(MakeJSON(body)))
+    }).catch(e => console.log(e)),
 }
 //=======================================================================|
 function APIErr(res, message, code) {
@@ -23,17 +24,17 @@ function APIErr(res, message, code) {
 function APISucc(res, message) {
     return res.status(200).json(message);
 }
-router.get("/vault/:section", function(req, res) {
+router.get("/Get-Trending", function(req, res) {
     API.get("videos", {
-        part:"contentDetails",
+        part:"snippet",
         chart: "mostPopular",
         regionCode: "US",
         maxResults: 25
-    }).then(d => APISucc(res, {data: d}),e => APIErr(res, {message: "Could not fetch Popular Videos", error: e}))
+    }).then(d => APISucc(res, {data: d.items}),e => APIErr(res, {message: "Could not fetch Popular Videos", error: e}))
 });
 
 router.use(function(req, res, next) {
-    APIErr(res, "Invalid API call", API_C.Invalid);
+    APIErr(res, "Invalid API call", 403);
 });
 
 module.exports = router;
